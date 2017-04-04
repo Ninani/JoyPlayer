f2571fc2bde0170833c5c69f0194643223d9bc5f
Łukasz Gurdek <lukasz.gurdek@node1.com>
2017-04-04 22:06:24 +0200

    Play button

--- frontend/app/components/App.js
+++ frontend/app/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 import axios from 'axios';
+import authorize from 'oauth2-implicit'
+import spotify from 'spotify-web-api-js';
 
 export default class App extends Component {
   handleSend = () => {
@@ -16,11 +18,43 @@ export default class App extends Component {
     );
   }
 
+  componentDidMount = () => {
+    this.spotifyClient = new spotify();
+
+    if(window.location.href.includes('access_token')) {
+      this.handleAuthorize()
+    }
+  }
+
+  handleAuthorize = () => {
+    const credentials = authorize({
+      auth_uri: 'https://accounts.spotify.com/authorize',
+      client_id: '1bd9ce6008794d0d81b61dc5c100ef86',
+      redirect_uri: 'http://localhost:8080',
+      scope: ['user-read-private', 'user-read-email', 'user-modify-playback-state'],
+    })
+    if(credentials) {
+      this.spotifyClient.setAccessToken(credentials.accessToken)
+      this.spotifyClient.getMe().then(function(data) {
+        console.log('Me: ', data);
+      }, function(err) {
+        console.error(err);
+      });
+      this.spotifyClient.playTracks({ uris: ['spotify:track:2dJVX40gxhBSPfoPP8nmRo'] }).then(function(data) {
+        console.log(data);
+      }, function(err) {
+        console.error(err);
+      });
+
+    }
+  }
+
   render() {
     return (
       <div>
         <Webcam ref={(c) => { this.webcam = c; }} audio={false} />
         <button onClick={this.handleSend}>Send</button>
+        <button onClick={this.handleAuthorize}>Play something nice</button>
       </div>
     );
   }
--- frontend/app/config/Root.js
+++ frontend/app/config/Root.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, useRouterHistory } from 'react-router';
+import { Router, Route, useRouterHistory, browserHistory } from 'react-router';
 import { createHashHistory } from 'history';
 import shortid from 'shortid';
 
@@ -11,8 +11,9 @@ const history = useRouterHistory(createHashHistory)({ queryKey: false });
 export default class Root extends Component {
   render() {
     return (
-      <Router history={history} key={shortid.generate()}>
+      <Router history={browserHistory}>
         <Route path="/" component={App} />
+        <Route path="/*" component={App} />
       </Router>
     );
   }
