bc6e609cd9e5ce97f7a219c6e2e624068cf839bc
Piotr Ociepka <pkociepka@gmail.com>
2017-05-29 23:43:37 +0200

    Mock for track endpoint

--- backend/server.py
+++ backend/server.py
@@ -12,5 +12,16 @@ def get_emotion():
     return recognize_emotion(image_data)
 
 
+@app.route("/track/<string:mood>", methods=["GET"])
+def get_track(mood):
+    if mood == "happy":
+        return "6NPVjNh8Jhru9xOmyQigds"
+    elif mood == "sadness":
+        return "5Q30xdABnojqN3wBIhrsQp"
+    elif mood == "anger":
+        return "1sV6neOqXX4jglsFUz6QX9"
+    elif mood == "neutral":
+        return "2w9w4X0qTu81r4jJ1afU8E"
+
 if __name__ == "__main__":
     app.run(host="localhost", port=int("8081"))
